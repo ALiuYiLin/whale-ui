a708208c623476ae5bc355f0e3b581c50e114536
ALinYiLin <scliuyilin@163.com>
2025-06-21 20:00:09 +0800

    add readme file

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -6,9 +6,10 @@ import path from "path";
 
 const epPackage = path.resolve(__dirname,"../packages/whale-ui/package.json")
 const epOutput = path.resolve(__dirname,"../dist/whale-ui")
-
+const readme = path.resolve(__dirname,"../README.md")
 export const copyFiles = () => Promise.all([
   copyFile(epPackage,path.join(epOutput,'package.json')),
+  copyFile(readme,path.join(epOutput,'README.md')),
 ])
 
 
