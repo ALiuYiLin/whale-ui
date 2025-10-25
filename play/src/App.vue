92be6cd1d82d1b037c17b4b15cc1f71e36a8710b
ALinYiLin <scliuyilin@163.com>
2025-10-25 15:39:06 +0800

    refact:init

---
+++ play/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {WlButton} from '@wahle-ui/components'
+</script>
+
+<template>
+  <div>
+    <WlButton>11111</WlButton>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
