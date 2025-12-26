a6f8599f0b16350a7f5db32add0bd7c263f1b82c
ALiuYiLin <scliuyilin@163.com>
2025-12-26 17:37:12 +0800

    feat: tailwind v4->v3

--- docs/.vitepress/config/vite.ts
+++ docs/.vitepress/config/vite.ts
@@ -2,13 +2,18 @@ import { UserConfig } from "vitepress";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import { MarkdownTransform } from "../plugins/markdown-transforms";
 import path, { resolve } from "path";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from  "tailwindcss"
+import autoprefixer from "autoprefixer"
 
 
 type ViteConfig = Required<UserConfig>["vite"];
 
 export const viteConfig: ViteConfig = {
   css: {
+    postcss: {
+      plugins: [autoprefixer, tailwindcss],
+    },
+    
     preprocessorOptions: {
       scss: {
         api: "modern-compiler",
@@ -17,7 +22,6 @@ export const viteConfig: ViteConfig = {
     },
   },
   plugins: [
-    tailwindcss(),
     MarkdownTransform(),
     createSvgIconsPlugin({
       iconDirs: [path.resolve(__dirname, "../vitepress/assets/icons")],
